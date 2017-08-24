56233fd6cd95483b58328743311443df56b8a2e7
shusiwei <shusiwei@yeah.net>
2017-08-24 14:14:18 +0800

    init

---
+++ 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print(100+200+300)
+name = input('input your name:')
+print('我的名字是：', name)
+
+count = 100
+if count >= 100:
+    print('>=100')
+else:
+    print('>100')
+
+print(1 == 1)
+
+print('''line1
+line2
+line3''')
+
+def plus(a, b):
+    return a + b
+
+print(plus(1, 2))
